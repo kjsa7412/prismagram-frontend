d92c463507f1379451c93d4be6fd532f48462fba
Kyungjin Sa <48097915+kjsa7412@users.noreply.github.com>
2019-04-23 21:53:41 +0900

    #5.5 Toastify and createAccount Mutation

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import styled, { ThemeProvider } from "styled-components";
+import { useQuery } from "react-apollo-hooks";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import Router from "./Router";
-import { useQuery } from "react-apollo-hooks";
 import Footer from "./Footer";
 
 const QUERY = gql`
@@ -12,24 +14,22 @@ const QUERY = gql`
     isLoggedIn @client
   }
 `;
-
 const Wrapper = styled.div`
   margin: 0 auto;
   max-width: 935px;
   width: 100%;
 `;
-
 export default () => {
   const {
     data: { isLoggedIn }
   } = useQuery(QUERY);
-
   return (
     <ThemeProvider theme={Theme}>
       <Wrapper>
         <GlobalStyles />
         <Router isLoggedIn={isLoggedIn} />
         <Footer />
+        <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
       </Wrapper>
     </ThemeProvider>
   );
